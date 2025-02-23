464d2ed6e2baf3da7e6f8dd0de512b1ccb69b997
pappukumar8521 <mrpappusahu4841@gmail.com>
2025-02-23 23:46:56 +0530

    [Pappu kuamr] Add. Add REST endpoints for Employee CRUD operations

---
+++ EmployeePayrollAppApplicatio/src/main/java/EmployeePayrollAppApplicatio/EmployeePayrollAppApplicatio/EmployeePayrollAppApplicatioApplication.java
@@ -0,0 +1,13 @@
+package EmployeePayrollAppApplicatio.EmployeePayrollAppApplicatio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollAppApplicatioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayrollAppApplicatioApplication.class, args);
+	}
+
+}
---
+++ EmployeePayrollAppApplicatio/src/test/java/EmployeePayrollAppApplicatio/EmployeePayrollAppApplicatio/EmployeePayrollAppApplicatioApplicationTests.java
@@ -0,0 +1,13 @@
+package EmployeePayrollAppApplicatio.EmployeePayrollAppApplicatio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayrollAppApplicatioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
