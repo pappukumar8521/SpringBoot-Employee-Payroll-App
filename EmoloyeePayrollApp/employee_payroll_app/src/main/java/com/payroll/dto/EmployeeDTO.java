16c6666237cbd24a434f8bf77e344fb84a964934
pappukumar8521 <mrpappusahu4841@gmail.com>
2025-02-27 22:59:19 +0530

    [Pappu kumar]  Add. Introducing DTO model to AddressingBook App

---
+++ EmoloyeePayrollApp/employee_payroll_app/src/main/java/com/payroll/dto/EmployeeDTO.java
@@ -0,0 +1,13 @@
+package com.payroll.dto;
+
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeeDTO {
+    private String name;
+    private double salary;
+}
+
