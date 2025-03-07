27af689a41f1e27be7bd4caf683c56dcc27360a3
pappukumar8521 <mrpappusahu4841@gmail.com>
2025-03-07 15:41:30 +0530

    [Pappu kumar] Add. Add Validation to Name Field so the REST call can be validated

---
+++ EmoloyeePayrollApp/employee_payroll_app/src/main/java/com/payroll/dto/EmployeeDTO.java
@@ -0,0 +1,18 @@
+package com.payroll.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EmployeeDTO {
+
+    @NotBlank(message = "Name cannot be empty")
+    @Pattern(regexp = "^[A-Z][a-zA-Z ]{2,}$", message = "Name must start with a capital letter and have at least 3 characters")
+    private String name;
+
+    private int age;
+    private String department;
+}
