b00bab29058f72d5d23c66da56d7497777081548
pappukumar8521 <mrpappusahu4841@gmail.com>
2025-03-07 11:34:54 +0530

    [Pappu kumar] Add. Determine the Logging Levels, Logging to Console or File, Logging Patterns,

---
+++ EmoloyeePayrollApp/employee_payroll_app/src/main/java/com/payroll/dto/EmployeeDTO.java
@@ -0,0 +1,15 @@
+package com.payroll.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EmployeeDTO {
+    private String name;
+    private int age;
+    private String department;
+}
+
+
---
+++ EmoloyeePayrollApp/employee_payroll_app/src/main/java/com/payroll/model/Employee.java
@@ -0,0 +1,55 @@
+package com.payroll.model;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @Min(value = 1000, message = "Salary must be at least 1000")
+    private double salary;
+
+    public Employee() {}
+
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public Employee(Object o, String name, double salary) {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+
+}
